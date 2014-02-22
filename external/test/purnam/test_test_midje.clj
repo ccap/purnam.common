d1e41dd80698f6fb2a6da49a61dfcfb097493927
Chris Zheng <chris.zheng@auspost.com.au>
2014-02-22 22:08:42 +1100

    Fixing midje error msg

--- external/test/purnam/test_test_midje.clj
+++ external/test/purnam/test_test_midje.clj
@@ -13,13 +13,13 @@
 
 (fact "fact-groups"
   (fact-groups '(1 => 1))
-  => [[:purnam.test.midje/is 1 1 "1" "1"]]
+  => [[:purnam.test.midje/is 1 1 "'1'" "'1'"]]
 
   (fact-groups '(1 1 => 1 1))
   => [[:purnam.test.midje/norm 1]
-      [:purnam.test.midje/is 1 1 "1" "1"]
+      [:purnam.test.midje/is 1 1 "'1'" "'1'"]
       [:purnam.test.midje/norm 1]])
 
 (fact "fact-fn"
   (fact-fn {} '(1 1 => 1 1))
-  => '(purnam.test/describe {} (purnam.test/it "" {} 1 (purnam.test/is 1 1 "1" "1") 1)))
+  => '(purnam.test/describe {} (purnam.test/it "" {} 1 (purnam.test/is 1 1 "'1'" "'1'") 1)))
